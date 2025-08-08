526484732f8588390a4e7328119e5f274ec7b7b0
Jeff Cope <jeff@roadyscorp.com>
2025-08-07 18:52:51 -0600

    first autoloader instance

--- public/index.php
+++ public/index.php
@@ -1,1 +1,9 @@
-Hello, PHP World
+<?php
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+
+use JeffCode\Config;
+
+Config::sayHi();
+
+?>
+Hello, PHP World.
---
+++ src/Config.php
@@ -0,0 +1,10 @@
+<?php
+namespace JeffCode;
+
+class Config
+{
+	public static function sayHi(): void
+	{
+		echo "Heyoo, Woild.".PHP_EOL;
+	}
+}
