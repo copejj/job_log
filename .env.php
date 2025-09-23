353f0beceebdf48743097452806cb13cc70d761e
Jeff Cope <92327203+copejj@users.noreply.github.com>
2025-09-23 16:20:02 -0600

    Delete .env.php

--- .env.php
+++
@@ -1,8 +0,0 @@
-<?php return [
-	'ENVIRONMENT' => 'dev',
-	'DB_HOST' => 'localhost',
-	'DB_RO_HOST' => 'localhost',
-	'DB_USER' => 'jjcope',
-	'DB_PASS' => 'zappy1971',
-	'DB_NAME' => 'postgres',
-];
