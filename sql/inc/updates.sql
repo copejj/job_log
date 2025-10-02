34f677c31c91ab26ba537af899719878cddf81b4
Jeff Cope <jeff@roadyscorp.com>
2025-10-02 11:30:37 -0600

    fix for missing table

--- sql/inc/updates.sql
+++ sql/inc/updates.sql
@@ -1,2 +1,13 @@
-insert into config (name, value)
-values ('INVITE_INTERVAL', '24 hours');
+CREATE TABLE IF NOT EXISTS public.invites
+(
+    invite_id serial NOT NULL,
+    created_date timestamp without time zone NOT NULL DEFAULT now(),
+    key text COLLATE pg_catalog."default" NOT NULL DEFAULT "substring"(md5((random())::text), 1, 40),
+    user_id integer,
+    first_name text COLLATE pg_catalog."default",
+    last_name text COLLATE pg_catalog."default",
+    email text COLLATE pg_catalog."default",
+    CONSTRAINT invites_pkey PRIMARY KEY (invite_id)
+)
+
+TABLESPACE pg_default;
