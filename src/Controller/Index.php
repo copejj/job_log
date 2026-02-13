c573cdca1b8313defcb6d686473792dfc6fee761
Jeff Cope <jeff.cope@gmail.com>
2026-02-13 05:20:27 -0700

    Modifying index page

--- src/Controller/Index.php
+++ src/Controller/Index.php
@@ -24,9 +24,15 @@ class Index extends HeaderedContent
 					different tech and essentially exercising my brain while sometimes beating my head against a wall.  
 				</p>
 				<p>
-					This page is a Java page using the Spring-Boot framework on top of some Thymeleaf templates.  You
-					can find out more in the specs in the <a href="/?page=about">About</a> section if interested.  I have also put a fair amount
-					of effort into a PHP <strong>Brain Dribble</strong> over <a href="<?=$externalUrl?>" target="blank">HERE</a> if interested.
+					This page is a PHP page kind of home rolled in a semi-informed MVC style structure.  You
+					can find out more in the specs below if interested. I have also put a fair amount of effort into 
+					a Java version as well. 
+				</p>
+				<p>
+					<ul>
+						<li> <a href="/?page=about">PHP Server Specs</a> </li>
+						<li> <a href="<?=$externalUrl?>" target="blank">Java Brain Dribble</a> </li>
+					</ul>
 				</p>
 			</div>
 			<div class="about-image"></div>
